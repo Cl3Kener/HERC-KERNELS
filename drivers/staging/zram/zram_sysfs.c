edbf79833dc6823b7b65edec8b9848079b154187
Chester Kener <Cl3Kener@gmail.com>
2013-10-30 14:25:08 -0400

    Fix zram
    * Use my current uber-NO-BLN branch to fix this....

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -15,7 +15,6 @@
 #include <linux/device.h>
 #include <linux/genhd.h>
 #include <linux/mm.h>
-#include <linux/kernel.h>
 
 #include "zram_drv.h"
 
@@ -30,9 +29,18 @@ static u64 zram_stat64_read(struct zram *zram, u64 *v)
 	return val;
 }
 
-static inline struct zram *dev_to_zram(struct device *dev)
+static struct zram *dev_to_zram(struct device *dev)
 {
-	return (struct zram *)dev_to_disk(dev)->private_data;
+	int i;
+	struct zram *zram = NULL;
+
+	for (i = 0; i < num_devices; i++) {
+		zram = &devices[i];
+		if (disk_to_dev(zram->disk) == dev)
+			break;
+	}
+
+	return zram;
 }
 
 static ssize_t disksize_show(struct device *dev,
@@ -46,12 +54,13 @@ static ssize_t disksize_show(struct device *dev,
 static ssize_t disksize_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
+	int ret;
 	u64 disksize;
 	struct zram *zram = dev_to_zram(dev);
 
-	disksize = memparse(buf, NULL);
-	if (!disksize)
-		return -EINVAL;
+	ret = kstrtoull(buf, 10, &disksize);
+	if (ret)
+		return ret;
 
 	down_write(&zram->init_lock);
 	if (zram->init_done) {
@@ -73,13 +82,6 @@ static ssize_t initstate_show(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 
 	return sprintf(buf, "%u\n", zram->init_done);
-}
-
-static inline ssize_t initstate_store(struct device *dev,
-				      struct device_attribute *attr,
-				      const char *buf, size_t len)
-{
-	return 0;
 }
 
 static ssize_t reset_store(struct device *dev,
@@ -184,18 +186,17 @@ static ssize_t mem_used_total_show(struct device *dev,
 	u64 val = 0;
 	struct zram *zram = dev_to_zram(dev);
 
-	down_read(&zram->init_lock);
 	if (zram->init_done) {
-		val = zs_get_total_size_bytes(zram->mem_pool);
+		val = xv_get_total_size_bytes(zram->mem_pool) +
+			((u64)(zram->stats.pages_expand) << PAGE_SHIFT);
 	}
-	up_read(&zram->init_lock);
 
 	return sprintf(buf, "%llu\n", val);
 }
 
 static DEVICE_ATTR(disksize, S_IRUGO | S_IWUSR,
 		disksize_show, disksize_store);
-static DEVICE_ATTR(initstate, S_IRUGO | S_IWUSR, initstate_show, initstate_store);
+static DEVICE_ATTR(initstate, S_IRUGO, initstate_show, NULL);
 static DEVICE_ATTR(reset, S_IWUSR, NULL, reset_store);
 static DEVICE_ATTR(num_reads, S_IRUGO, num_reads_show, NULL);
 static DEVICE_ATTR(num_writes, S_IRUGO, num_writes_show, NULL);
